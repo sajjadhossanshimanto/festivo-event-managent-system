4e1ac98abc3b44e7da2ed8d17dfcf4b01431f755
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-08-08 21:26:31 +0600

    django debug toolbar installed

--- festivo/urls.py
+++ festivo/urls.py
@@ -19,10 +19,13 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
+from debug_toolbar.toolbar import debug_toolbar_urls
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('events.urls')),
 ]
 
+urlpatterns += debug_toolbar_urls()
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
