896fc400c10aa924a86c16936cdd88d2df5b095f
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-08-12 23:47:49 +0600

    usercreatetion form

--- users/forms.py
+++ users/forms.py
@@ -11,13 +11,22 @@ DEFAULT_CLASSES = (
 class UserForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ['username', 'first_name', 'last_name',
-                  'password1', 'password2', 'email']
+        fields = [
+            'username', 'first_name', 'last_name',
+            'email', 'password1', 'password2'
+        ]
 
-class SignUpForm(UserCreationForm):
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'password1', 'password2']
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs.update({
+                'class': 'w-full px-4 py-2 mt-1 border rounded-lg '
+                         'focus:outline-none focus:ring-2 focus:ring-blue-500',
+                'placeholder': field.label
+            })
+
+            if field.help_text:
+                field.help_text = f'<span class="block mt-1 text-sm text-gray-500">{field.help_text}</span>'
 
 class LoginForm(AuthenticationForm):
     pass
--- users/views.py
+++ users/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
 
-from users.forms import SignUpForm, LoginForm
+from users.forms import LoginForm
 # from users.models import User
 from users.forms import UserForm
 
@@ -50,14 +50,14 @@ def participant_delete(request, id):
 
 def signup_view(request):
     if request.method == "POST":
-        form = SignUpForm(request.POST)
+        form = UserForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
             return redirect('dashboard')
     else:
-        form = SignUpForm()
-    return render(request, 'users/signup.html')
+        form = UserForm()
+    return render(request, 'users/signup.html', {'form': form})
 
 def login_view(request):
     if request.method == "POST":
